9c10fb00af39ad48d6d0b01fedac4846f1546882
Santosh Humagain <santoshhumagain1997@gmail.com>
2024-03-30 23:13:58 -0400

    add two

--- Santosh/HelloWorld.java
+++ Santosh/HelloWorld.java
@@ -1,7 +1,11 @@
 package Santosh;
 
 public class HelloWorld {
-    public static void main(String []args) {
+    public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int sum, a = 7, b = 9;
+        sum = a + b;
+        System.out.println(sum);
     }
 }
