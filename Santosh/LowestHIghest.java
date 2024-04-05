64bfb3bee310792091a20e73bc83a60ffae90ee2
Santosh Humagain <santoshhumagain1997@gmail.com>
2024-04-05 03:28:04 -0400

    hcf

--- Santosh/LowestHIghest.java
+++ Santosh/LowestHIghest.java
@@ -3,12 +3,26 @@ package Santosh;
 public class LowestHIghest {
 //Lowest com factor
     //highest common factor
-    int firstNum = 4, secondNum = 7;
 
 
+    public static void main(String[] args) {
+        int firstNum = 20, secondNum = 5, c = 0;
+        int hcf = 1;
+
+        int x = Math.max(firstNum, secondNum);
+        for (int i = 2; i <= x / 2; i++) {
+
+            if (firstNum % i == 0 && secondNum % i == 0) {
+                hcf = i;
+            }
+        }
+        System.out.println(hcf);
 
 
+        // lcm
 
+
+    }
 
 
 }
