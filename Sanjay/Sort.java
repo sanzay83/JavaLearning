77a23a96e54f528c8cd2431d919e46b1edade8a7
Sanjay Duwal <sanzay8342258@gmail.com>
2024-04-07 12:31:48 -0500

    Sort algorithm added

---
+++ Sanjay/Sort.java
@@ -0,0 +1,21 @@
+package Sanjay;
+
+import java.util.Arrays;
+
+public class Sort {
+
+    public static int[] Sorted(int[] arr) {
+        int arrayLength = arr.length;
+        int i, j, temp;
+        for(i=0; i < arrayLength-1; i++) {
+            for (j = i + 1; j < arrayLength; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
