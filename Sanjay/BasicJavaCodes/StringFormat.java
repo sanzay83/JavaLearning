10db95f8f1a3031b5f41aafd81e05d667e8604a0
Sanjay Duwal <sanzay8342258@gmail.com>
2024-04-16 23:20:18 -0500

    test

--- Sanjay/BasicJavaCodes/StringFormat.java
+++ Sanjay/BasicJavaCodes/StringFormat.java
@@ -1,12 +1,14 @@
 package Sanjay.BasicJavaCodes;
 
 public class StringFormat {
+
     public static void main(String[] args) {
         String name = "Sanjay";
         int num = 1200;
         System.out.printf("Hello %s and number is %d %n", name, num);
         System.out.printf("Hello %S and number is %010d %n", name, num);
-        System.out.printf("Hello %S and number is %,d", name, num);
+        System.out.printf("Hello %S and number is %,d %n", name, num);
+        System.out.printf("Hello %S and number is %-10d spaces", name, num);
 
         /*
         %s is for string
--- Sanjay/ClassAndObject/Student.java
+++ Sanjay/ClassAndObject/Student.java
@@ -1,2 +1,23 @@
-package Sanjay.ClassAndObject;public class Student {
+package Sanjay.ClassAndObject;
+
+public class Student {
+    final String name;  // final can only be assigned once in constructor only
+    final int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Student stu = new Student("Sanjay", 27);
+        System.out.println(stu);
+    }
+
+    @Override
+    public String toString() {
+        return "Student Details: {name:" + name + ", age:" + age + "}";
+    }
+
+
 }
