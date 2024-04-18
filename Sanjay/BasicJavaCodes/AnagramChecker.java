2d0c83eefaeb1be837f0a52582a90c2ea3dd9e00
Sanjay Duwal <sanzay8342258@gmail.com>
2024-04-17 23:48:15 -0500

    AnagramChecker

---
+++ Sanjay/BasicJavaCodes/AnagramChecker.java
@@ -0,0 +1,25 @@
+package Sanjay.BasicJavaCodes;
+
+import java.util.Arrays;
+
+public class AnagramChecker {
+    public static void main(String[] args) {
+        String firstString = "Racae".toLowerCase();
+        String secondString = "Carae".toLowerCase();
+
+        char[] firstArr = firstString.toCharArray();
+        char[] secondArr = secondString.toCharArray();
+
+        Arrays.sort(firstArr);
+        Arrays.sort(secondArr);
+
+        if (firstString.length() == secondString.length()) {
+            if (Arrays.equals(firstArr, secondArr)) {
+                System.out.println(true);
+            }
+
+        } else {
+            System.out.println(false);
+        }
+    }
+}
