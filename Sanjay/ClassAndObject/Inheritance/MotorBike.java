77c27e726ece75fd3a1578ec0805146b38d14df1
Sanjay Duwal <sanzay8342258@gmail.com>
2024-04-25 22:47:54 -0500

    super

--- Sanjay/ClassAndObject/Inheritance/MotorBike.java
+++ Sanjay/ClassAndObject/Inheritance/MotorBike.java
@@ -1,6 +1,12 @@
 package Sanjay.ClassAndObject.Inheritance;
 
 public class MotorBike extends TwoWheeler {
+    MotorBike() {
+        super();
+    }
 
-
+    @Override
+    public int getNoOfWheels() {
+        return super.getNoOfWheels();
+    }
 }
--- Sanjay/ClassAndObject/Inheritance/Vehicles.java
+++ Sanjay/ClassAndObject/Inheritance/Vehicles.java
@@ -4,6 +4,9 @@ public abstract class Vehicles implements Transport {
     // Car, motorbike, cycle, truck
     private int noOfWheels, noOfDoors, sideMirrors, maxSpeed;
     private String modelNumber, brand;
+    Vehicles() {
+        System.out.println("THis is vehicle");
+    }
 
     public void moving() {
         System.out.println("Vehicle is moving");
