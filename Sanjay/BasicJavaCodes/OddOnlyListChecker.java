9c5ba3903ccf6c74b25b72528fe990dde0840dd5
Sanjay Duwal <sanzay8342258@gmail.com>
2024-04-27 00:03:50 -0500

    Odd Only List Checker

---
+++ Sanjay/BasicJavaCodes/OddOnlyListChecker.java
@@ -0,0 +1,19 @@
+package Sanjay.BasicJavaCodes;
+
+public class OddOnlyListChecker {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 1, 4, 5, 4};
+        int[] arr2 = {1, 3, 5, 7};
+        System.out.println(OddOnlyChecker(arr));
+        System.out.println(OddOnlyChecker(arr2));
+    }
+
+    private static boolean OddOnlyChecker(int[] arr) {
+        for (int each : arr) {
+            if (each % 2 == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
