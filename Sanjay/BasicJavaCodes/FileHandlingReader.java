b53209f4e6f05df2ae51dc5c72d4dca1a0859252
Sanjay Duwal <sanzay8342258@gmail.com>
2024-04-30 01:17:53 -0500

    FileHandling try catch

---
+++ Sanjay/BasicJavaCodes/FileHandlingReader.java
@@ -0,0 +1,23 @@
+package Sanjay.BasicJavaCodes;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileHandlingReader {
+    public static void main(String[] args) {
+        String fileName = "java.txt";
+
+        try (FileReader reader = new FileReader(fileName)) {
+            int read = 0;
+            do {
+                read = reader.read();
+                System.out.print((char) read);
+            } while (read != -1);
+        } catch (FileNotFoundException e) {
+            System.out.printf("%s not found", fileName);
+        } catch (IOException e) {
+            System.out.printf("Exception orrcured: %s", e.getMessage());
+        }
+    }
+}
---
+++ Sanjay/BasicJavaCodes/FileHandlingV2.java
@@ -0,0 +1,18 @@
+package Sanjay.BasicJavaCodes;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandlingV2 {
+    public static void main(String[] args) {
+        String filename = "java.txt";
+
+        try (FileWriter writer = new FileWriter(filename);) {
+            writer.write("This is test file.");
+            writer.flush();
+            System.out.println("File Written Successfully");
+        } catch (IOException e) {
+            System.out.printf("Exception occured %s\n", e);
+        }
+    }
+}
